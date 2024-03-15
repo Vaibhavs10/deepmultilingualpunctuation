c28637d899fed81b1809c1d3694c43889065caf2
Vaibhav Srivastav <vaibhavs10@gmail.com>
2024-03-15 17:00:20 +0100

    up.

--- deepmultilingualpunctuation/punctuationmodel.py
+++ deepmultilingualpunctuation/punctuationmodel.py
@@ -4,23 +4,23 @@ import re
 import torch
 
 class PunctuationModel():
-    def __init__(self, model = "oliverguhr/fullstop-punctuation-multilang-large") -> None:        
+    def __init__(self, model = "oliverguhr/fullstop-punctuation-multilang-large") -> None:
         if torch.cuda.is_available():
-            self.pipe = pipeline("ner",model, aggregation_strategy="none", device=0)
+            self.pipe = pipeline("ner",model, aggregation_strategy="none", device=1)
         else:
-            self.pipe = pipeline("ner",model, aggregation_strategy="none")        
+            self.pipe = pipeline("ner",model, aggregation_strategy="none")
 
     def preprocess(self,text):
-        #remove markers except for markers in numbers 
-        text = re.sub(r"(?<!\d)[.,;:!?](?!\d)","",text) 
+        #remove markers except for markers in numbers
+        text = re.sub(r"(?<!\d)[.,;:!?](?!\d)","",text)
         #todo: match acronyms https://stackoverflow.com/questions/35076016/regex-to-match-acronyms
         text = text.split()
         return text
 
-    def restore_punctuation(self, text, chunk_size=230):        
+    def restore_punctuation(self, text, chunk_size=230):
         result = self.predict(self.preprocess(text), chunk_size)
         return self.prediction_to_text(result)
-        
+
     def overlap_chunks(self,lst, n, stride=0):
         """Yield successive n-sized chunks from lst with stride length of overlap."""
         for i in range(0, len(lst), n-stride):
@@ -33,33 +33,33 @@ class PunctuationModel():
 
         batches = list(self.overlap_chunks(words,chunk_size,overlap))
 
-        # if the last batch is smaller than the overlap, 
+        # if the last batch is smaller than the overlap,
         # we can just remove it
         if len(batches[-1]) <= overlap:
             batches.pop()
 
-        tagged_words = []     
+        tagged_words = []
         for batch in batches:
             # use last batch completely
-            if batch == batches[-1]: 
+            if batch == batches[-1]:
                 overlap = 0
             text = " ".join(batch)
-            result = self.pipe(text)      
+            result = self.pipe(text)
             assert len(text) == result[-1]["end"], "chunk size too large, text got clipped"
-                
+
             char_index = 0
             result_index = 0
-            for word in batch[:len(batch)-overlap]:                
+            for word in batch[:len(batch)-overlap]:
                 char_index += len(word) + 1
                 # if any subtoken of an word is labled as sentence end
-                # we label the whole word as sentence end        
+                # we label the whole word as sentence end
                 label = "0"
                 while result_index < len(result) and char_index > result[result_index]["end"] :
                     label = result[result_index]['entity']
                     score = result[result_index]['score']
-                    result_index += 1                        
+                    result_index += 1
                 tagged_words.append([word,label, score])
-        
+
         assert len(tagged_words) == len(words)
         return tagged_words
 
@@ -73,7 +73,7 @@ class PunctuationModel():
                 result += label+" "
         return result.strip()
 
-if __name__ == "__main__":    
+if __name__ == "__main__":
     model = PunctuationModel()
 
     text = "das , ist fies "
